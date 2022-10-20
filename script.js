4596a8430197991924d941483c12b4a6c6f6a251
Govind Harsh Sharma <95496933+ghsharma@users.noreply.github.com>
2022-10-20 07:02:57 +0530

    UI upgrade

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings:["Software-Developer.","Fullstack-Developer.","Designer.", "Freelancer."],
+        strings:["Web-Developer.","Software-Developer.","Designer.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings:["Software-Developer","Fullstack-Developer","Designer.", "Freelancer."],
+        strings:["Web-Developer.","Software-Developer.","Designer.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
