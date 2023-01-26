15772c165366cd0b02a403d76c7c86ac4f6e1fa3
Govind Harsh Sharma <95496933+ghsharma@users.noreply.github.com>
2023-01-27 04:32:09 +0530

    resume updated

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings:["Web-Developer.","Software-Developer.","Designer.", "Freelancer."],
+        strings:["Web-Developer.","Software-Developer.","Web-Designer.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings:["Web-Developer.","Software-Developer.","Designer.", "Freelancer."],
+        strings:["Web-Developer.","Software-Developer.","Web-Designer.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
