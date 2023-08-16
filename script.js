f9308773cf1b4e9fa43eace4747d67dc8988006d
Govind Harsh Sharma <govindharsh42@gmail.com>
2023-08-16 23:59:24 +0530

    skills

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings:["Web-Developer.","Software-Developer.","Web-Designer.", "Freelancer."],
+        strings: ["Web-Developer.", "Software-Developer.", "Web-Designer.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings:["Web-Developer.","Software-Developer.","Web-Designer.", "Freelancer."],
+        strings: ["Web-Developer.", "Software-Developer.", "Web-Designer.", "Freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -56,15 +56,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
@@ -82,11 +82,11 @@ let btn = document.getElementById("btn-letsgo");
 let header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  stars.style.left = value * 0.25 + "px";
-  moon.style.top = value * 1.05 + "px";
-  mountains_back.style.top = value * 0.5 + "px";
-  mountains_front.style.top = value * 0 + "px";
-  btn.style.marginTop = value * 1 + "px";
-  header.style.top = value * 0.5 + "px";
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + "px";
+    moon.style.top = value * 1.05 + "px";
+    mountains_back.style.top = value * 0.5 + "px";
+    mountains_front.style.top = value * 0 + "px";
+    btn.style.marginTop = value * 1 + "px";
+    header.style.top = value * 0.5 + "px";
 });
